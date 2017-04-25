559aa93a4ef59a95a6f6b862f03135d67be6f2a7
Johnny Eugênio <johnnybarroso711@hotmail.com>
2017-04-25 09:13:56 -0300

    Added Tin Lizzard storage
    Added Tin Lizzard storage

--- data/lib/miscellaneous/051-storages.lua
+++ data/lib/miscellaneous/051-storages.lua
@@ -898,6 +898,7 @@ Storage = {
 	-- Promotion Storage cannot be changed, it is set in source code
 	Promotion = 30018,
 	Uniwheel = 54321,
+	Tinlizzard = 54322,
 	SwampDiggingTimeout = 50143,
 	HydraEggQuest = 320,
 	Dragonfetish = 504,
