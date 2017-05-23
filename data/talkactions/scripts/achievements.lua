0079a95b5d0ea97d78be77d9888e5bdb48f0a314
Xavier Carreón <darkjav_88@hotmail.com>
2017-05-22 22:17:09 -0500

    Fix talkaction, only GOD can do it.

--- data/talkactions/scripts/achievements.lua
+++ data/talkactions/scripts/achievements.lua
@@ -1,4 +1,12 @@
 function onSay(player, words, param)
+	
+	if not player:getGroup():getAccess() then
+		return true
+	end
+
+	if player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return false
+	end
 		
 	if words == "/addach" then
 		local p = param:split(",")
