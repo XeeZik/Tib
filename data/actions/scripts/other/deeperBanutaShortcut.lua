05c9cabd95e06d3fb2f8676ce7aebfbc0a707170
Luan Luciano <luanluciano@outlook.com>
2017-05-28 17:08:31 -0300

    Update deeperBanutaShortcut.lua

--- data/actions/scripts/other/deeperBanutaShortcut.lua
+++ data/actions/scripts/other/deeperBanutaShortcut.lua
@@ -3,10 +3,11 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return false
 	end
 
-	if player:getStorageValue(Storage.DeeperBanutaShortcut) ~= 1 then
-		player:removeItem(10523, 1)
-		player:setStorageValue(Storage.DeeperBanutaShortcut, 1)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You discovered a secret tunnel.")
+	if player:getStorageValue(Storage.DeeperBanutaShortcut) ~= 1 and player:removeItem(10523, 1) then
+		if player:removeItem(10523, 1) then
+			player:setStorageValue(Storage.DeeperBanutaShortcut, 1)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You discovered a secret tunnel.")
+		end
 	else
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have already discovered this secret.")
 	end
