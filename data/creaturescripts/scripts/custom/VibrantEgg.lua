588709b4a5652ed79b487e75363eef9cd2833ff8
Luan Luciano <luanluciano@outlook.com>
2017-05-28 17:36:08 -0300

    Update VibrantEgg.lua

--- data/creaturescripts/scripts/custom/VibrantEgg.lua
+++ data/creaturescripts/scripts/custom/VibrantEgg.lua
@@ -10,12 +10,12 @@ function onModalWindow(player, modalWindowId, buttonId, choiceId)
 			return false
 		end
 		
-		if not player:removeItem(26194, 1) then 
+		if player:removeItem(26194, 1) then 
+			player:addMount(mounts[choiceId].ID)
+			player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_YELLOW)
+		else	
 			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must have a Vibrant Egg in your backpack!")
 			return false
 		end
-
-		player:addMount(mounts[choiceId].ID)
-		player:getPosition():sendMagicEffect(CONST_ME_FIREWORK_YELLOW)
 	end
 end
