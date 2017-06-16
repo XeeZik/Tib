2c5f20c442af590d7cdac0a69c65d97a2ad2ead8
Erick Nunes <erickzenho@live.com>
2017-06-15 21:10:23 -0300

    fix Capture the Flag Event

--- data/spells/scripts/support/invisible.lua
+++ data/spells/scripts/support/invisible.lua
@@ -7,5 +7,8 @@ condition:setParameter(CONDITION_PARAM_TICKS, 200000)
 combat:setCondition(condition)
 
 function onCastSpell(creature, variant)
+if (getPlayerStorageValue(cid, _CTF_LIB.teamssto) > 0) then
+return doPlayerSendCancel(cid, "Você não pode usar invisible durante o CTF!") and doSendMagicEffect(getThingPos(cid), 2)
+end
 	return combat:execute(creature, variant)
 end
