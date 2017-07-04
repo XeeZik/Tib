b89c2175a7eb425a89501ae911711658d8ae9c74
Erick Nunes <erickzenho@live.com>
2017-07-03 21:14:08 -0300

    updated items.otb 11.31 and removed some errors in console

--- data/creaturescripts/scripts/others/recompense.lua
+++ data/creaturescripts/scripts/others/recompense.lua
@@ -4,7 +4,7 @@ local table =
 	[40] = {type = "bank", id = 10000, msg = "You have received 10000 gold in your bank for advancing to Level 40."},
 	[50] = {type = "bank", id = 10000, msg = "You have received 10000 gold in your bank for advancing to Level 50."},
 	[75] = {type = "bank", id = 10000, msg = "You have received 10000 gold in your bank for advancing to Level 75."},
-	[100] = {type = "bank", id = 40000 msg = "You have received 40000 gold in your bank for advancing to Level 100."},
+	[100] = {type = "bank", id = 40000, msg = "You have received 40000 gold in your bank for advancing to Level 100."},
 	[150] = {type = "item", id = 5942, msg = "You have received one blessed wooden stake because you reached level 150."},
 	[200] = {type = "item", id = 2195, msg = "You have been awarded with 1 boots of haste for reaching level 200."},
 }
