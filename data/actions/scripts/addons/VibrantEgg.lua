8e48fc118db3657afe1b0dfb0bc15dd30351c08f
Erick Nunes <erickzenho@live.com>
2017-07-05 12:24:23 -0300

    fix mounts

--- data/actions/scripts/addons/VibrantEgg.lua
+++ data/actions/scripts/addons/VibrantEgg.lua
@@ -1,37 +1,13 @@
-local mounts = {
-         
-	[1] = {name = "Sparkion", ID = 94},
-	
-    
-    }
- function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
-    player:registerEvent("VibrantEgg")
- 
-    local title = "Choose your mount!"
-    local message = "You will receive the mount you select!"
- 
-    local window = ModalWindow(1001, title, message)
-        if player:getItemCount(26194) >= 1 then 
-                window:addButton(100, "Confirm")
-                window:setDefaultEnterButton(100)
-        else
-                window:setDefaultEnterButton(101)
+function onUse(cid, item, fromPosition, itemEx, toPosition, isHotkey)
+local player = Player(cid)
+if not player:hasMount(94) then
+player:addMount(94)
+player:getPosition():sendMagicEffect(15)
+item:remove(1)
+doCreatureSay(cid, "You receive the permission to ride a sparkion.", TALKTYPE_ORANGE_1)
+else
+player:getPosition():sendMagicEffect(3)
+player:sendTextMessage(MESSAGE_INFO_DESCR, "You already have this mount.")
 end
-    window:addButton(101, "Cancel")
-    window:setDefaultEscapeButton(101)
-   
-    for i = 1, #mounts do
-                local o = mounts[i].name
-                if not player:hasMount(mounts[i].ID) then
-                        window:addChoice(i, o)
-                end
-    end
-       
-        if window:getChoiceCount() == 0 then
-        window:setMessage("YOU HAVE ALL MOUNTS, THEN CLICK CANCEL!")
-                --add achievement
-    end
- 
-    window:sendToPlayer(player)
-    return true
+return true
 end
