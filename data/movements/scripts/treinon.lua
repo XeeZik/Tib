bb2e5aea60fce716da3e662b41f5ba7cb8f0f067
Xavier Carreón <darkjav_88@hotmail.com>
2017-07-11 18:15:09 -0500

    Fix stamina training online
    Must be each 2 minutes.

--- data/movements/scripts/treinon.lua
+++ data/movements/scripts/treinon.lua
@@ -10,13 +10,13 @@ local function rechargeStamina(cid)
 
     player:setStamina(player:getStamina() + 1)
 
-    eventsId[cid] = addEvent(rechargeStamina, 60 * 1000, cid)
+    eventsId[cid] = addEvent(rechargeStamina, 2 * 60 * 1000, cid)
 end
 
 function onStepIn(creature, item, position, fromPosition)
     if creature:isPlayer() then
         local cid = creature:getId()
-        eventsId[cid] = addEvent(rechargeStamina, 60 * 1000, cid)
+        eventsId[cid] = addEvent(rechargeStamina, 2 * 60 * 1000, cid)
  		creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Attack the Monk and you will win 1 of stamina every 2 minutes training here.')
     end
 
