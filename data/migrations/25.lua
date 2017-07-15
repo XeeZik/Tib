55a9c7a00d2c3f62721c8adfa5c76cd8fcc31cec
Xavier Carreón <darkjav_88@hotmail.com>
2017-07-14 23:17:38 -0500

    Database migrations for Blessings Dialog System

--- data/migrations/25.lua
+++ data/migrations/25.lua
@@ -1,5 +1,5 @@
 function onUpdateDatabase()
-	print("> Updating database to version 23 (Imbuement and Prey System)")
+	print("> Updating database to version 25 (Imbuement and Prey System)")
 	db.query("CREATE TABLE IF NOT EXISTS `player_prey` (`player_id` int(11) NOT NULL, `name` varchar(50) NOT NULL, `mindex` smallint(6) NOT NULL, `mcolumn` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
 	db.query("CREATE TABLE IF NOT EXISTS `player_preytimes` (`player_id` int(11) NOT NULL, `bonus_type1` int(11) NOT NULL, `bonus_value1` int(11) NOT NULL, `bonus_name1` varchar(50) NOT NULL, `bonus_type2` int(11) NOT NULL, `bonus_value2` int(11) NOT NULL, `bonus_name2` varchar(50) NOT NULL, `bonus_type3` int(11) NOT NULL, `bonus_value3` int(11) NOT NULL, `bonus_name3` varchar(50) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;")
 	db.query("ALTER TABLE `players` ADD COLUMN `prey_stamina_1` int(11) DEFAULT NULL")
