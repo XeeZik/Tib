ccc799275a4b88232dc09a7dc06a3730b88a7365
Matt Gomez <mattyx14@users.noreply.github.com>
2017-07-18 21:40:51 -0500

    database migrations/27.lua

--- data/migrations/27.lua
+++ data/migrations/27.lua
@@ -1,3 +1,6 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 27 (Fix bonus reroll)")
+	db.query("ALTER TABLE `players` DROP COLUMN `bonus_reroll`")
+	db.query("ALTER TABLE `players` ADD COLUMN `bonus_reroll` int(11) NOT NULL DEFAULT '0'")
+	return true
 end
