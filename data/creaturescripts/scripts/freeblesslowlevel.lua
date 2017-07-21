ca430039a55d08fb446c11a3af1ac26d12977df9
DESKTOP-47GQ5SM\Charles <eternalscripts@gmail.com>
2017-07-21 18:03:30 -0300

    Fix free bless.

--- data/creaturescripts/scripts/freeblesslowlevel.lua
+++ data/creaturescripts/scripts/freeblesslowlevel.lua
@@ -3,43 +3,12 @@ local freeBlessMaxLevel = 50
 function onLogin(cid)
     local player = Player(cid)
     if player:getLevel() <= freeBlessMaxLevel then
-    
-	if player:hasBlessing(1) then
-	else
-    player:addBlessing(1, 1)
-	end
-	
-	if player:hasBlessing(2) then
-	else
-    player:addBlessing(2, 1)
-	end
-	
-	if player:hasBlessing(3) then
-	else
-    player:addBlessing(3, 1)
-	end
-	
-	if player:hasBlessing(4) then
-	else
-    player:addBlessing(4, 1)
-	end
-	if player:hasBlessing(5) then
-	else
-    player:addBlessing(5, 1)
-	end
-	if player:hasBlessing(6) then
-	else
-    player:addBlessing(6, 1)
-	end
-	if player:hasBlessing(7) then
-	else
-    player:addBlessing(7, 1)
-	end
-	if player:hasBlessing(8) then
-	else
-    player:addBlessing(8, 1)
-	end
-	
+    	for i = 1, 8 do
+    		if not player:hasBlessing(i) then
+    			player:addBlessing(i, 1)
+    		end
+    	end
+
     	player:sendTextMessage(MESSAGE_EVENT_ADVANCE,'You received all blessing for you to be level less than ' .. freeBlessMaxLevel .. '!')
         player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
     end
