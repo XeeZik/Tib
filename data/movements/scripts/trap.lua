5daba873c740b13af6dccd26ad19429a9bf1d95e
João Leonardo Pereira <jlcvp@cin.ufpe.br>
2017-07-21 13:20:53 -0300

    fixing trap behavior on pvp and non-pvp worlds fixes #213

--- data/movements/scripts/trap.lua
+++ data/movements/scripts/trap.lua
@@ -1,7 +1,7 @@
 local traps = {
 	[1510] = {transformTo = 1511, damage = {-50, -100}},
 	[1513] = {damage = {-50, -100}},
-	[2579] = {transformTo = 2578, damage = {-15, -30}, ignorePlayer = false},
+	[2579] = {transformTo = 2578, damage = {-15, -30}, ignorePlayer = (Game.getWorldType() == WORLD_TYPE_NO_PVP) },
 	[4208] = {transformTo = 4209, damage = {-15, -30}, type = COMBAT_EARTHDAMAGE}
 }
 
@@ -11,12 +11,12 @@ function onStepIn(creature, item, position, fromPosition)
 		return true
 	end
 
-	if trap.transformTo then
-		item:transform(trap.transformTo)
-	end
-
 	if trap.ignorePlayer and creature:isPlayer() then
 		return true
+	end
+
+	if trap.transformTo then
+		item:transform(trap.transformTo)
 	end
 
 	doTargetCombatHealth(0, creature, trap.type or COMBAT_PHYSICALDAMAGE, trap.damage[1], trap.damage[2], CONST_ME_NONE)
