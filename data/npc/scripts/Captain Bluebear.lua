0cb9e27a7939764f6689922520f3127230e7a3de
Andreeyyy <andreypetris@gmail.com>
2017-07-21 20:13:54 -0300

    FIX FreeBlessLowLevel and NPC Bluebear THAIS
    - Free bless don't multiple blessing onLogin
    - Bluebear don't accept free account.

--- data/npc/scripts/Captain Bluebear.lua
+++ data/npc/scripts/Captain Bluebear.lua
@@ -13,7 +13,7 @@ npcHandler:addModule(VoiceModule:new(voices))
 -- Travel
 local function addTravelKeyword(keyword, cost, destination, action)
 	local travelKeyword = keywordHandler:addKeyword({keyword}, StdModule.say, {npcHandler = npcHandler, text = 'Do you seek a passage to ' .. keyword:titleCase() .. ' for |TRAVELCOST|?', cost = cost, discount = 'postman'})
-		travelKeyword:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = false, cost = cost, discount = 'postman', destination = destination}, nil, action)
+		travelKeyword:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = true, cost = cost, discount = 'postman', destination = destination}, nil, action)
 		travelKeyword:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, text = 'We would like to serve you some time.', reset = true})
 end
 
