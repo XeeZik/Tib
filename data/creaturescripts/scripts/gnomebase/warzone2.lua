76b661369d6ce09e689a3781523a19d8ba336f28
Matt Gomez <mattyx14@users.noreply.github.com>
2017-07-21 16:13:01 -0500

    only lines and coment :P

--- data/creaturescripts/scripts/gnomebase/warzone2.lua
+++ data/creaturescripts/scripts/gnomebase/warzone2.lua
@@ -1,66 +1,67 @@
---local function getCreaturePosition(creature)
---    return creature:getPosition()
---end
+--[[
+local function getCreaturePosition(creature)
+	return creature:getPosition()
+end
+]]
 
 function onKill(creature, target)
-    local targetMonster = target:getMonster()
-    if not targetMonster or targetMonster:getName():lower() ~= 'parasite' then
-        return true
-    else
-        local target = targetMonster
+	local targetMonster = target:getMonster()
+	if not targetMonster or targetMonster:getName():lower() ~= 'parasite' then
+		return true
+	else
+		local target = targetMonster
+		pos = {
+			{ x = 33097, y = 31976, z = 11 },
+			{ x = 33097, y = 31977, z = 11 },
+			{ x = 33097, y = 31978, z = 11 },
+			{ x = 33097, y = 31979, z = 11 }
+		}
+		barrier = false
+		for i = 1, 4 do
+			if (getCreaturePosition(target).x == pos[i].x and 
+				getCreaturePosition(target).y == pos[i].y and 
+				getCreaturePosition(target).z == pos[i].z) then
+					barrier = true
+			end
+		end
 
-        pos = {
-            { x = 33097, y = 31976, z = 11 },
-            { x = 33097, y = 31977, z = 11 },
-            { x = 33097, y = 31978, z = 11 },
-            { x = 33097, y = 31979, z = 11 }
-        }
-        barrier = false
-        for i = 1, 4 do
-            if (getCreaturePosition(target).x == pos[i].x and
-                    getCreaturePosition(target).y == pos[i].y and
-                    getCreaturePosition(target).z == pos[i].z) then
-                barrier = true
-            end
-        end
+		last = false
+		if (barrier == true) then
+			crystals = {
+				{ x = 33098, y = 31976, z = 11 },
+				{ x = 33098, y = 31977, z = 11 },
+				{ x = 33098, y = 31978, z = 11 },
+				{ x = 33098, y = 31979, z = 11 }
+			}
 
-        last = false
-        if (barrier == true) then
-            crystals = {
-                { x = 33098, y = 31976, z = 11 },
-                { x = 33098, y = 31977, z = 11 },
-                { x = 33098, y = 31978, z = 11 },
-                { x = 33098, y = 31979, z = 11 }
-            }
+			local crystalItem = Tile(crystals[1]):getTopTopItem()
+			if (crystalItem:getId() == 18459) then
+				for i = 1, 4 do
+					crystalItem = Tile(crystals[i]):getTopTopItem()
+					crystalItem:transform(18460)
+					crystalItem:decay()
+				end
+			elseif (crystalItem:getId() == 18460) then
+				for i = 1, 4 do
+					crystalItem = Tile(crystals[i]):getTopTopItem()
+					crystalItem:transform(18461)
+					crystalItem:decay()
+				end
+			elseif (crystalItem:getId() == 18461) then
+				for i = 1, 4 do
+					crystalItem = Tile(crystals[i]):getTopTopItem()
+					crystalItem:remove()
+					addEvent(doCreateItem, 30 * 1000, 18459, 1, crystals[i])
+				end
+				last = true
+			end
 
-            local crystalItem = Tile(crystals[1]):getTopTopItem()
+			if (barrier == true and last == true) then
+				setGlobalStorageValue(3144, 1)
+				addEvent(setGlobalStorageValue, 30 * 60 * 1000, 3144, 0)
+			end
+		end
+	end
 
-            if (crystalItem:getId() == 18459) then
-                for i = 1, 4 do
-                    crystalItem = Tile(crystals[i]):getTopTopItem()
-                    crystalItem:transform(18460)
-                    crystalItem:decay()
-                end
-            elseif (crystalItem:getId() == 18460) then
-                for i = 1, 4 do
-                    crystalItem = Tile(crystals[i]):getTopTopItem()
-                    crystalItem:transform(18461)
-                    crystalItem:decay()
-                end
-            elseif (crystalItem:getId() == 18461) then
-                for i = 1, 4 do
-                    crystalItem = Tile(crystals[i]):getTopTopItem()
-                    crystalItem:remove()
-                    addEvent(doCreateItem, 30 * 1000, 18459, 1, crystals[i])
-                end
-                last = true
-            end
-
-            if (barrier == true and last == true) then
-                setGlobalStorageValue(3144, 1)
-                addEvent(setGlobalStorageValue, 30 * 60 * 1000, 3144, 0)
-            end
-        end
-    end
-    return true
+	return true
 end
