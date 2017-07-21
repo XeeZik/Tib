a3e43a08b247bbc170c1a5703a12733dabfd30ca
viperthepala <tim@steenwelle.com>
2017-07-21 11:08:01 +0200

    Update record.lua

--- data/globalevents/scripts/record.lua
+++ data/globalevents/scripts/record.lua
@@ -1,4 +1,4 @@
 function onRecord(current, old)
-	addEvent(Game.broadcastMessage, 150, 'Novo recorde: ' .. current .. ' jogadores online.', MESSAGE_STATUS_DEFAULT)
+	addEvent(Game.broadcastMessage, 150, 'New record: ' .. current .. ' players online.', MESSAGE_STATUS_DEFAULT)
 	return true
 end
