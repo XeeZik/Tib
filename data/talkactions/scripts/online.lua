30da7609f95dac295b2fe01c5e6c46e058a739f9
marsoncschneider <marsoncschneider@gmail.com>
2017-07-23 12:51:35 -0300

    temporary mod

--- data/talkactions/scripts/online.lua
+++ data/talkactions/scripts/online.lua
@@ -3,7 +3,7 @@ function onSay(player, words, param)
 	local players = Game.getPlayers()
 	local playerCount = Game.getPlayerCount()--+69 -- spoofing
 
-	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, playerCount .. " players online.")
+	player:sendTextMessage(MESSAGE_INFO_DESCR, playerCount .. " players online.")
 
 	--local i = 0
 --	local msg = ""
