d2ee41256d942253f00dbff180059d86788caedf
marsoncschneider <marsoncschneider@gmail.com>
2017-07-23 12:52:12 -0300

    Party protection

--- data/events/scripts/creature.lua
+++ data/events/scripts/creature.lua
@@ -85,7 +85,7 @@ function Creature:onTargetCombat(target)
 		if self:getStorageValue(_Lib_Battle_Info1.TeamOne1.storage1) >= 1 and target:getStorageValue(_Lib_Battle_Info1.TeamOne1.storage1) >= 1 or self:getStorageValue(_Lib_Battle_Info1.TeamTwo1.storage1) >= 1 and target:getStorageValue(_Lib_Battle_Info1.TeamTwo1.storage1) >= 1 then
             return RETURNVALUE_YOUMAYNOTATTACKTHISPLAYER
         end
-		--party protection
+		--party protect
 		local party = self:getParty()
 		if party then
 			local targetParty = target:getParty()
