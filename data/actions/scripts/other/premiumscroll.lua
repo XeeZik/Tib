72978549fce22233ee7ddd8d80a6af45fbc888f3
marsoncschneider <marsoncschneider@gmail.com>
2017-07-23 20:57:15 -0300

    update world coins

--- data/actions/scripts/other/premiumscroll.lua
+++ data/actions/scripts/other/premiumscroll.lua
@@ -1,6 +1,20 @@
+ITEM_TIBIA_COIN = 24774
+ITEM_PREMIUM_SCROLL = 16101
+
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:addPremiumDays(30)
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have received 30 premium days.")
-	item:remove(1)
+	if player:isPlayer() then
+		if item:getId() == ITEM_PREMIUM_SCROLL then
+			local coins = 30
+			player:addTibiaCoins(coins)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have received " .. coins .. " tibia coins.")
+			item:remove(1)
+		elseif item:getId() == ITEM_TIBIA_COIN then
+			local coins = player:getItemCount(ITEM_TIBIA_COIN)
+			player:addTibiaCoins(coins)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have received " .. coins .. " tibia coins.")
+			item:remove(coins)
+		end
+		
+	end
 	return true
 end
